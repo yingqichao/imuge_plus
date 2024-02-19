0bbc1d2341a11787339d92f1ae27a5b6b9bc1da8
QichaoYing <shinydotcom@163.com>
2024-02-19 14:50:59 +0800

    2024

--- leetcode/greater.py
+++ leetcode/greater.py
@@ -1,14 +1,14 @@
-from collections import deque
+import pandas as pd
+import matplotlib.pyplot as plt
 
-class Solution(object):
+file_path = "./athlete_events.csv"
+athlete_events_df = pd.read_csv(file_path)
 
-    def nextGreaterElements(self, nums):
-        q = deque()
+age_distribution = athlete_events_df[athlete_events_df['Age'].notna()]
 
-
-
-
-
-if __name__ == '__main__':
-    s = Solution()
-    print(s.nextGreaterElements([1,2,3,4,3]))
+plt.figure(figsize=(12, 8))
+age_distribution.boxplot(column='Age', by='Sport', vert=False, patch_artist=True, showfliers=False)
+plt.title('Age Distribution of Athletes by Sport')
+plt.xlabel('Age')
+plt.ylabel('Sport')
+plt.show()
